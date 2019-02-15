a45bfcf5ba739f86a3b624b643a63ba6c2e0a724
Vitalii <9455469@mail.ru>
2019-02-15 18:30:29 +0300

    2.2

--- index.php
+++ index.php
@@ -82,39 +82,47 @@
 				var planeGeo = new THREE.PlaneBufferGeometry( 100.1, 100.1 );
 				// reflector/mirror plane
 				
-				var mirrorRTT = new THREE.ReflectorRTT(new THREE.PlaneBufferGeometry( 100, 100 ), {clipBias: 0.003, textureWidth:WIDTH, textureHeight:HEIGHT});
+				var planeBottom = new THREE.Mesh( planeGeo, new THREE.MeshLambertMaterial() );
+				planeBottom.rotation.set(0, 0, 0);
+				scene.add( planeBottom );				
 				
-				var mirrorRTT_bottom = new THREE.ReflectorRTT(new THREE.PlaneBufferGeometry( 100, 100 ), {clipBias: 0.003, textureWidth:WIDTH, textureHeight:HEIGHT});
-				var mirrorRTT_right = new THREE.ReflectorRTT(new THREE.PlaneBufferGeometry( 100, 100 ), {clipBias: 0.003, textureWidth:WIDTH, textureHeight:HEIGHT});
+				var planeRight = new THREE.Mesh( planeGeo, new THREE.MeshLambertMaterial() );
+				planeRight.position.x = 50;
+				planeRight.position.y = 50;
+				planeRight.rotateY( - Math.PI / 2 );
+				scene.add( planeRight );
 				
-				mirrorRTT_right.position.x = 50;
-				mirrorRTT_right.position.y = 50;
-				mirrorRTT_right.rotateY( - Math.PI / 2 );
-				scene.add( mirrorRTT_right );
-				
-				
-				mirrorRTT_bottom.position.x = 0;
-				mirrorRTT_bottom.position.y = 0;
-				mirrorRTT_bottom.rotateX( - Math.PI / 2 );
-				scene.add( mirrorRTT_bottom );
-				
-				
-				rightMirrorMaterial = new THREE.PhongNodeMaterial();
-				rightMirrorMaterial.color.r = 0; 
-				rightMirrorMaterial.color.g = 0; 
-				rightMirrorMaterial.color.b = 0; 
-				rightMirrorMaterial.environment = new THREE.ReflectorNode( mirrorRTT_right ); 
-				//rightMirrorMaterial.defaultAttributeValues.color = [0.5, 0.5, 0.5];
-					console.log(rightMirrorMaterial);			
+				var planeBack = new THREE.Mesh( planeGeo, new THREE.MeshPhongMaterial( { color: 0xffffff } ) );
+				planeBack.position.z = - 50;
+				planeBack.position.y = 50;
+				scene.add( planeBack );	
+
+				var planeLeft = new THREE.Mesh( planeGeo, new THREE.MeshPhongMaterial( { color: 0xff0000 } ) );
+				planeLeft.position.x = - 50;
+				planeLeft.position.y = 50;
+				planeLeft.rotateY( Math.PI / 2 );
+				scene.add( planeLeft );				
 				
 				
-				var groundMirrorMaterial_2 = new THREE.PhongNodeMaterial();
-				groundMirrorMaterial_2.environment = new THREE.ReflectorNode( mirrorRTT_bottom ); 				
+		var mirrorRTT = new THREE.ReflectorRTT(new THREE.PlaneBufferGeometry( 100, 100 ), {clipBias: 0.003, textureWidth:WIDTH, textureHeight:HEIGHT});
+		mirrorRTT.position.copy(planeBottom.position);
+		mirrorRTT.rotation.copy(planeBottom.rotation);
+		scene.add( mirrorRTT );
+		var material = new THREE.PhongNodeMaterial();
+		material.environment = new THREE.ReflectorNode( mirrorRTT ); 	
+		planeBottom.material = material;		
+		
+		
+		var mirrorRTT = new THREE.ReflectorRTT(new THREE.PlaneBufferGeometry( 100, 100 ), {clipBias: 0.003, textureWidth:WIDTH, textureHeight:HEIGHT});
+		mirrorRTT.position.copy(planeRight.position);
+		mirrorRTT.rotation.copy(planeRight.rotation);
+		scene.add( mirrorRTT );
+		var material = new THREE.PhongNodeMaterial();
+		material.environment = new THREE.ReflectorNode( mirrorRTT ); 	
+		planeRight.material = material;			
 				
 				
-				var mirrorMesh_2 = new THREE.Mesh( planeGeo, groundMirrorMaterial_2 );
-				mirrorRTT_bottom.add( mirrorMesh_2 );
-				
+
 				
 				
 
@@ -144,38 +152,19 @@
 				planeTop.position.y = 100;
 				planeTop.rotateX( Math.PI / 2 );
 				scene.add( planeTop );
-				var planeBack = new THREE.Mesh( planeGeo, new THREE.MeshPhongMaterial( { color: 0xffffff } ) );
-				planeBack.position.z = - 50;
-				planeBack.position.y = 50;
-				scene.add( planeBack );
+
 				var planeFront = new THREE.Mesh( planeGeo, new THREE.MeshPhongMaterial( { color: 0x7f7fff } ) );
 				planeFront.position.z = 50;
 				planeFront.position.y = 50;
 				planeFront.rotateY( Math.PI );
 				scene.add( planeFront );
-				var planeRight = new THREE.Mesh( planeGeo, rightMirrorMaterial );
-				planeRight.position.x = 50;
-				planeRight.position.y = 50;
-				planeRight.rotateY( - Math.PI / 2 );
-				scene.add( planeRight );
-				var planeLeft = new THREE.Mesh( planeGeo, new THREE.MeshPhongMaterial( { color: 0xff0000 } ) );
-				planeLeft.position.x = - 50;
-				planeLeft.position.y = 50;
-				planeLeft.rotateY( Math.PI / 2 );
-				scene.add( planeLeft );
+
+
 				// lights
 				var mainLight = new THREE.PointLight( 0xcccccc, 1.5, 250 );
 				mainLight.position.y = 60;
 				scene.add( mainLight );
-				var greenLight = new THREE.PointLight( 0x00ff00, 0.25, 1000 );
-				greenLight.position.set( 550, 50, 0 );
-				scene.add( greenLight );
-				var redLight = new THREE.PointLight( 0xff0000, 0.25, 1000 );
-				redLight.position.set( - 550, 50, 0 );
-				scene.add( redLight );
-				var blueLight = new THREE.PointLight( 0x7f7fff, 0.25, 1000 );
-				blueLight.position.set( 0, 50, 550 );
-				scene.add( blueLight );
+
 			}
 			
 			
